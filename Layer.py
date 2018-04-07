f593a202a3748aa476468dceb4e04ecdb06caed4
Jean-Baptiste Gardel <jbaptiste.gardel@gmail.com>
2018-04-07 17:23:07 +0200

    nbNeuron

--- Layer.py
+++ Layer.py
@@ -2,20 +2,20 @@ class Layer:
     """Class defining the layers found in a network:
         - its type (dense layer, Convolutional layer ...) called type
         - a list of Neurons called neuronList
-        - a number a neuron called nNeuron
+        - a number a neuron called nbNeuron
         - its activation function called activationFun
         - a network (in which this layer is) 
         - an index being the index of the layer in its network"""
     
-    def __init__(self, typeLayer, activationFunction, nNeuron, network):
+    def __init__(self, typeLayer, activationFunction, nbNeuron, network):
         """neuronList is initialized as void, activationFunction is activationFun, the index is set refering to network, the first layer has index 0"""
         self.type=typeLayer
         self.neuronList=[]
         self.activationFun=activationFunction
-        self.nNeuron=nNeuron
+        self.nbNeuron=nbNeuron
         self.network=network
         self.layerIndex=len(self.network.getLayerList())-1
-        for j in range(self.nNeuron):
+        for j in range(self.nbNeuron):
             myNeuron=Neuron(self,j)
             self.neuronList.append(myNeuron)
     
